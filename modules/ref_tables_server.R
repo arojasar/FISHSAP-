c6ec21f62bf2b4ca6185ec35d987a2d765e2828a
arojasar <70030947+arojasar@users.noreply.github.com>
2025-03-17 08:39:36 -0500

    Update ref_tables_server.R

--- modules/ref_tables_server.R
+++ modules/ref_tables_server.R
@@ -1,147 +1,96 @@
 # modules/ref_tables_server.R
 
-library(shiny)
-library(DT)
-
-ref_tables_server <- function(id, central_conn, table_name) {
+ref_tables_server <- function(id, central_conn = NULL, table_name) {
   moduleServer(id, function(input, output, session) {
-    message("Entrando en ref_tables_server para ID: ", id)
-    
-    # Mapear nombres de tablas a nombres en IndexedDB
-    table_mapping <- list(
-      "Sitios de Desembarque" = "sitios",
-      "Especies Comerciales" = "especies",
-      "Categorías de Estado" = "estados",
-      "Clasificación" = "clasifica",
-      "Grupos" = "grupos",
-      "Subgrupos" = "subgrupo",
-      "Artes de Pesca" = "arte",
-      "Método de Técnica de Pesca" = "metodo",
-      "Métodos de Propulsión" = "propulsion",
-      "Área de Pesca" = "area",
-      "Subárea de Pesca" = "subarea",
-      "Registradores de Campo" = "registrador",
-      "Embarcaciones" = "embarcaciones",
-      "Gastos de Faena" = "gastos",
-      "Valor Mensual de los Gastos" = "valor_mensual_gastos",
-      "TRM (Dólar)" = "trm_dolar"
-    )
-    
-    # Crear un valor reactivo para almacenar los datos de la tabla
-    table_data <- reactiveVal(data.frame(Message = "Esperando datos..."))
-    
-    # Enviar mensaje loadTableData solo cuando cambia la tabla seleccionada
-    observeEvent(table_name(), {
-      selected_table <- table_mapping[[table_name()]]
-      if (!is.null(selected_table)) {
-        message("Enviando mensaje loadTableData para tabla:", selected_table)
-        session$sendCustomMessage("loadTableData", list(table = selected_table))
-      }
-    })
-    
-    # Actualizar table_data cuando lleguen nuevos datos de IndexedDB
-    observeEvent(input$ref_table_output_data, {
-      message("Datos recibidos de IndexedDB:", input$ref_table_output_data)
-      if (is.null(input$ref_table_output_data)) {
-        table_data(data.frame(Message = "No hay datos disponibles para esta tabla"))
-      } else {
-        table_data(as.data.frame(input$ref_table_output_data))
-      }
-    }, ignoreNULL = FALSE)
-    
-    # Renderizar la tabla
+    ns <- session$ns
+
+    message("Entrando en ref_tables_server para ID:", id)
+
+    # Renderizar la tabla con datos de IndexedDB
     output$ref_table_output <- renderDT({
       message("Renderizando tabla en ref_tables_server dentro de renderDT...")
-      datatable(table_data(), options = list(pageLength = 5))
+      req(input$ref_table_output_data)  # Esperar datos desde IndexedDB
+      input$ref_table_output_data       # Mostrar datos dinámicos
     })
-    
-    # Formulario para agregar nuevos valores
-    output$ref_form <- renderUI({
-      message("Renderizando formulario para:", table_name())
-      selected_table <- table_mapping[[table_name()]]
-      if (is.null(selected_table)) return(NULL)
-      
-      tagList(
-        h4(paste("Agregar nuevo valor a", table_name())),
-        if (selected_table == "sitios") {
-          tagList(
-            textInput("sitios_CODSIT", "Código"),
-            textInput("sitios_NOMSIT", "Nombre"),
-            actionButton("save_ref_button", "Guardar", icon = icon("save"))
-          )
-        } else if (selected_table == "especies") {
-          tagList(
-            textInput("especies_CODESP", "Código"),
-            textInput("especies_Nombre_Comun", "Nombre Común"),
-            textInput("especies_Nombre_Cientifico", "Nombre Científico"),
-            selectInput("especies_Subgrupo_ID", "Subgrupo", choices = c("", "SUB01", "SUB02")),
-            selectInput("especies_Clasificacion_ID", "Clasificación", choices = c("", "CLA01", "CLA02")),
-            numericInput("especies_Constante_A", "Constante A", value = 0, step = 0.0000001),
-            numericInput("especies_Constante_B", "Constante B", value = 0, step = 0.0000001),
-            selectInput("especies_Clase_Medida", "Clase Medida", choices = c("cm", "mm")),
-            selectInput("especies_Clasificacion_Comercial", "Clasificación Comercial", choices = c("Rojo", "Negro")),
-            textInput("especies_Grupo", "Grupo"),
-            actionButton("save_ref_button", "Guardar", icon = icon("save"))
-          )
-        } else {
-          tagList(
-            textInput("generic_ID", "Código"),
-            textInput("generic_Nombre", "Nombre"),
-            actionButton("save_ref_button", "Guardar", icon = icon("save"))
-          )
-        }
-      )
-    })
-    
-    # Manejar el guardado de nuevos valores
-    observeEvent(input$save_ref_button, {
-      message("Botón Guardar (referencia) presionado. Enviando datos a IndexedDB...")
-      selected_table <- table_mapping[[table_name()]]
-      if (is.null(selected_table)) return()
-      
-      new_data <- if (selected_table == "sitios") {
-        list(
-          CODSIT = input$sitios_CODSIT,
-          NOMSIT = input$sitios_NOMSIT,
-          Creado_Por = "system",
-          Fecha_Creacion = as.character(Sys.time()),
-          Modificado_Por = "system",
-          Fecha_Modificacion = as.character(Sys.time()),
-          Sincronizado = 0
+
+    # Manejar el botón "Guardar"
+    observeEvent(input$save_button, {
+      message("Botón Guardar presionado en ref_tables_server. Enviando datos a IndexedDB...")
+      table <- table_name()
+      new_data <- list()
+
+      # Mapear nombres de tablas a campos
+      if (table == "Sitios de Desembarque") {
+        new_data <- list(
+          site_code = input$site_code,
+          site_name = input$site_name
         )
-      } else if (selected_table == "especies") {
-        list(
-          CODESP = input$especies_CODESP,
-          Nombre_Comun = input$especies_Nombre_Comun,
-          Nombre_Cientifico = input$especies_Nombre_Cientifico,
-          Subgrupo_ID = input$especies_Subgrupo_ID,
-          Clasificacion_ID = input$especies_Clasificacion_ID,
-          Constante_A = input$especies_Constante_A,
-          Constante_B = input$especies_Constante_B,
-          Clase_Medida = input$especies_Clase_Medida,
-          Clasificacion_Comercial = input$especies_Clasificacion_Comercial,
-          Grupo = input$especies_Grupo,
-          Creado_Por = "system",
-          Fecha_Creacion = as.character(Sys.time()),
-          Modificado_Por = "system",
-          Fecha_Modificacion = as.character(Sys.time()),
-          Sincronizado = 0
+      } else if (table == "Especies Comerciales") {
+        new_data <- list(
+          species_code = input$species_code,
+          common_name = input$common_name,
+          scientific_name = input$scientific_name,
+          constant_a = input$constant_a,
+          constant_b = input$constant_b
         )
-      } else {
-        list(
-          ID = input$generic_ID,
-          Nombre = input$generic_Nombre,
-          Creado_Por = "system",
-          Fecha_Creacion = as.character(Sys.time()),
-          Modificado_Por = "system",
-          Fecha_Modificacion = as.character(Sys.time()),
-          Sincronizado = 0
+      } else if (table == "Categorías de Estado") {
+        new_data <- list(
+          cat_code = input$cat_code,
+          cat_name = input$cat_name
+        )
+      } else if (table == "Clasificación") {
+        new_data <- list(
+          clas_code = input$clas_code,
+          clas_name = input$clas_name
+        )
+      } else if (table == "Subgrupos") {
+        new_data <- list(
+          subgrupo_code = input$subgrupo_code,
+          subgrupo_name = input$subgrupo_name
         )
       }
-      
-      session$sendCustomMessage("saveData", list(table = selected_table, data = new_data))
+
+      # Determinar el nombre de la tabla en IndexedDB
+      table_map <- list(
+        "Sitios de Desembarque" = "sitios",
+        "Especies Comerciales" = "especies",
+        "Categorías de Estado" = "categorias",
+        "Clasificación" = "clasifica",
+        "Subgrupos" = "subgrupo"
+      )
+      table_key <- table_map[[table]]
+
+      if (!is.null(table_key)) {
+        session$sendCustomMessage("saveData", list(table = table_key, data = new_data))
+
+        # Intentar sincronizar con Neon si hay conexión
+        if (!is.null(central_conn)) {
+          tryCatch({
+            form_df <- as.data.frame(new_data, stringsAsFactors = FALSE)
+            form_df$Sincronizado <- 0  # Añadir campo Sincronizado
+            dbWriteTable(central_conn, table_key, form_df, append = TRUE)
+            print("Datos guardados en Neon:", form_df)
+
+            # Actualizar estado de sincronización en IndexedDB
+            primary_key <- switch(table_key,
+              "sitios" = "site_code",
+              "especies" = "species_code",
+              "categorias" = "cat_code",
+              "clasifica" = "clas_code",
+              "subgrupo" = "subgrupo_code"
+            )
+            session$sendCustomMessage("updateSyncStatus", list(table = table_key, registro = new_data[[primary_key]], Sincronizado = 1))
+          }, error = function(e) {
+            print("Error al guardar en Neon:", e$message)
+          })
+        } else {
+          print("No hay conexión a Neon; datos guardados solo en IndexedDB.")
+        }
+      } else {
+        message("Tabla no reconocida para guardar:", table)
+      }
     })
-    
-    message("Saliendo de ref_tables_server para ID: ", id)
+
+    message("Saliendo de ref_tables_server para ID:", id)
   })
 }
